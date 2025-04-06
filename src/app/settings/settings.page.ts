d1c56f5d74dd0dc5d2c48394f3752cf5668e2fdf
David Alejandro Osorio Sánchez <david.osorio004@mymdc.net>
2025-04-06 15:41:32 -0500

    avances settings

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from 'src/components/footer/footer.component';
 import { NavbarComponent } from 'src/components/navbar/navbar.component';
+import { IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonLabel, IonDatetime, IonItem, IonInput, IonTextarea, IonButton, IonSelect, IonSelectOption, IonContent, IonAccordion } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [IonAccordion, IonButton, IonTextarea, IonInput, IonItem, IonDatetime, IonLabel, IonCardContent, IonCardHeader, IonCardTitle, IonCard, NavbarComponent, FooterComponent, IonSelectOption, IonSelect, IonContent],
 })
 export class SettingsPage implements OnInit {
+  navigateTo(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 
